1ab3ffecbb8ae6b7dabbd77deb19ab886cf47b52
Hzkiyas Kebede <kebedehzkiyas@gmail.com>
2026-01-17 04:18:53 +0000

    TASK1C: S1_SW_03 - simple calculator using switch

---
+++ js/task1c/S1_SW_03.js
@@ -0,0 +1,17 @@
+function calc(a, op, b) {
+  switch(op) {
+    case "+": return a + b;
+    case "-": return a - b;
+    case "*": return a * b;
+    case "/": return b === 0 ? null : a / b;
+    default: return null;
+  }
+}
+
+// Tests
+console.log(calc(5,"+",3));  // 8
+console.log(calc(5,"-",3));  // 2
+console.log(calc(5,"*",3));  // 15
+console.log(calc(6,"/",3));  // 2
+console.log(calc(6,"/",0));  // null
+console.log(calc(6,"%",3));  // null
