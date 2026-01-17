badc8c33ac7233a348ba102aad119036e4768cb6
Hzkiyas Kebede <kebedehzkiyas@gmail.com>
2026-01-17 04:46:27 +0000

    TASK2: S2_FOR_02 - find first even number

---
+++ js/task2b/S2_FOR_02.js
@@ -0,0 +1,10 @@
+function findFirstEven(nums) {
+  for (const n of nums) {
+    if (n % 2 === 0) return n;
+  }
+  return null;
+}
+
+// Tests
+console.log(findFirstEven([1,3,5,6,8])); // 6
+console.log(findFirstEven([1,3,5]));     // null
