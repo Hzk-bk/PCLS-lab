a962b325f002a4e431e20bcf6fb263323c7faf1b
Hzkiyas Kebede <kebedehzkiyas@gmail.com>
2026-01-17 04:25:11 +0000

    TASK2: S2_ARR_01 - clean strings to numbers, drop NaN

---
+++ js/task2/S2_ARR_01.js
@@ -0,0 +1,10 @@
+// S2_ARR_01.js
+function cleanNumbers(arr) {
+  return arr
+    .map(item => +item.trim())
+    .filter(num => !Number.isNaN(num));
+}
+
+// Tests
+console.log(cleanNumbers([" 1 ", "x", "2"])); // [1, 2]
+console.log(cleanNumbers(["3", " 4 ", "a", "5"])); // [3,4,5]
