718a56b430c21ba229d7ba3b653d5224036c21da
kebedehzkiyas <30435131-kebedehzkiyas@users.noreply.replit.com>
2026-01-17 02:13:13 +0000

    TASK1: S1_VAR_01 - declare var, let, const + print table

--- js/task1/S1_VAR_01.js
+++ js/task1/S1_VAR_01.js
@@ -1,1 +1,9 @@
+var myVar = "Hello";
+let myLet = 42;
+const myConst = true;
 
+console.table([
+  { name: "myVar", value: myVar, type: typeof myVar },
+  { name: "myLet", value: myLet, type: typeof myLet },
+  { name: "myConst", value: myConst, type: typeof myConst }
+]);
