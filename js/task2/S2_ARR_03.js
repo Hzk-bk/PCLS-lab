7fb2545754d4bd95d4a763aae5103b4c3bfe8850
Hzkiyas Kebede <kebedehzkiyas@gmail.com>
2026-01-17 04:31:45 +0000

    TASK2: S2_ARR_03 - chunk array into smaller arrays

---
+++ js/task2/S2_ARR_03.js
@@ -0,0 +1,13 @@
+function chunk(arr, size) {
+  if (size <= 0) return null;
+  const result = [];
+  for (let i=0; i<arr.length; i+=size) {
+    result.push(arr.slice(i, i+size));
+  }
+  return result;
+}
+
+// Tests
+console.log(chunk([1,2,3,4,5],2)); // [[1,2],[3,4],[5]]
+console.log(chunk([1,2,3],1));     // [[1],[2],[3]]
+console.log(chunk([1,2,3],0));     // null
